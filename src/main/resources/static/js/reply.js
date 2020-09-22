1f386870fb1f2bfcbd3ce8ada4f9737575d6e093
DongHee <donup08@naver.com>
2020-09-22 20:39:07 +0900

    reply 수정,삭제 화면 구현

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -6,7 +6,8 @@ var replyManger = (function () {
         var page = obj.page || 1;
         console.log(bno+"dd"+page);
         $.getJSON('/replies/pages/'+bno+"/"+page,callback);
-    }
+    };
+
     var add = function (obj, callback) {
         console.log("add........");
         var bno = obj.bno;
@@ -21,13 +22,37 @@ var replyManger = (function () {
             success:callback
         });
 
-    }
+    };
+
     var update = function (obj, callback) {
         console.log("update........");
-    }
+
+        var bno = obj.bno;
+        var page = obj.page || 1;
+
+        $.ajax({
+            type:'put',
+            url: '/replies/pages/'+bno+"/"+page,
+            data: JSON.stringify(obj),
+            dataType: 'json',
+            contentType: "application/json",
+            success:callback
+        });
+    };
+
     var remove = function (obj, callback) {
         console.log("get remove........");
-    }
+        var bno = obj.bno;
+        var rno=obj.rno;
+
+        $.ajax({
+            type:'delete',
+            url: '/replies/pages/'+bno+"/"+rno,
+            dataType: 'json',
+            contentType: "application/json",
+            success:callback
+        });
+    };
 
     return {
         getAll: getAll,
